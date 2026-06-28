e4907ef909ea2e17403d882b81ab7c1877171e3d
Kanata-Report <cathedral2998@yahoo.co.jp>
2026-06-28 14:17:57 +0900

    astroconfig変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,19 @@
 // astro.config.mjs
 
 import { defineConfig } from 'astro/config';
-import node from '@astrojs/node';
+// import node from '@astrojs/node';
 import react from '@astrojs/react';
 
-// import cloudflare from '@astrojs/cloudflare';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
 integrations: [react()], //検索機能,ハンバーガーメニュー
   output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+//   adapter: node({
+//     mode: 'standalone'
+//   }),
 
-//   adapter: cloudflare(),
+  adapter: cloudflare(),
   
   devToolbar: {// 開発ツールバーを無効化
     enabled: false,
